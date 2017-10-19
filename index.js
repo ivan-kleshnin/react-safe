d5f8f2cadc39c601c1ac46ed0d05ae7654988b1e
Ivan Kleshnin <ivan@paqmind.com>
2017-10-19 11:24:10 +0300

    Add a build step with ES5

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-let React = require("react")
-let htmlTags = require("html-tags")
+"use strict";
 
-let omit = (keys, obj) => {
-  let obj2 = {}
-  for (let key of Object.keys(obj)) {
-    if (!keys.includes(key)) {
-      obj2[key] = obj[key]
+var React = require("react");
+var htmlTags = require("html-tags");
+
+var omit = function omit(keys, obj) {
+  var obj2 = {};
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = Object.keys(obj)[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var key = _step.value;
+
+      if (!keys.includes(key)) {
+        obj2[key] = obj[key];
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
     }
   }
-  return obj2
-}
 
-let Safe = htmlTags.reduce((tags, tag) => {
-  tags[tag] = (props) => {
-    let children = props.children
-    delete props.children
-    return React.createElement(tag, Object.assign(
-      omit(["children"], props),
-      {dangerouslySetInnerHTML: {__html: props.children}}
-    ))
-  }
-  return tags
-}, {})
+  return obj2;
+};
 
-module.exports = Safe
+var Safe = htmlTags.reduce(function (tags, tag) {
+  tags[tag] = function (props) {
+    var children = props.children;
+    delete props.children;
+    return React.createElement(tag, Object.assign(omit(["children"], props), { dangerouslySetInnerHTML: { __html: props.children } }));
+  };
+  return tags;
+}, {});
+
+module.exports = Safe;
